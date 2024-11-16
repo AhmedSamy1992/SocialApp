c72ed7822491c1ef1876ab76e4b9779f5cfd5d2f
Ahmed.Samy <Ahmed.Samy@DEV16-PC2>
2024-11-16 17:19:46 +0200

    change claims extensions

--- DatingAppApi/Extensions/ClaimsPrincipleExtensions.cs
+++ DatingAppApi/Extensions/ClaimsPrincipleExtensions.cs
@@ -14,7 +14,7 @@ namespace DatingAppApi.Extensions
         public static int GetUserId(this ClaimsPrincipal user)
         {
             var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier)
-                ?? throw new Exception("Cannot get username from token"));
+                ?? throw new Exception("Cannot get userId from token"));
 
             return userId;
         }
