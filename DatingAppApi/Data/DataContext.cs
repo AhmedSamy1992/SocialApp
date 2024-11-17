da5669bdfd1f3a4d77e3d21fe7a53e612ac84e93
Ahmed.Samy <Ahmed.Samy@DEV16-PC2>
2024-11-17 22:26:01 +0200

    add Like entity - datacontext - migration

--- DatingAppApi/Data/DataContext.cs
+++ DatingAppApi/Data/DataContext.cs
@@ -8,5 +8,26 @@ namespace DatingAppApi.Data
     public class DataContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(K => new { K.SourceUserId, K.TargetUserId });
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.TargetUserId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
     }
 }
